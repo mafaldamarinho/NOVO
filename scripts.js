6b992c54a3b3129ce2e9258b533108da3649de15
mafaldamarinho <mafalda_marinhomr@hotmail.com>
2016-12-04 23:33:47 +0000

    secondcommit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+/*function writeName(f, s) {
+  var name = f + " " + s;
+  document.write(name);
+  document.write
+}
+
+var firstName = "john";
+
+var score= 10;
+if (score > 8){
+  document.write("TRUE");
+  }else{
+    document.write("FALSE");/*ignara a primeira premissa,, ve so a ultima dada/*
+  }
+
+  AULA DE JAVA SCRIPT-------27.OUT.-------*/
+/*aula 3nov---*/
+
+$('.btn').on('click', function(){
+  $('.title').addClass('active');
+});
+
+$('.btnum').on('click', function(){
+  $('.Item1').addClass('activeum');
+});
+
+$('.list li').on('click', function(){
+  $(this).addClass('active');/*$(this).next.addClass('active');----em vez de carregar no (this), carrega no aseguir a esse---*/
+});
